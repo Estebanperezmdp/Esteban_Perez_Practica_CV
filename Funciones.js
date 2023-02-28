cd23002e74cddbb9bc1efb1859d9f78c157f7243
Esteban Martin Perez <lagartosoy@gmail.com>
2023-02-28 19:48:04 -0300

    Sexto commit

--- Funciones.js
+++ Funciones.js
@@ -3,4 +3,15 @@ console.log("CV interaccion");
 document.getElementById("estadolaboral").onclick = function(){
     
     document.getElementById("resultado").innerHTML= "Estoy disponible, continuo desempleado, puedes completar el formulario presentado debajo. Muchas gracias!"
+};
+
+
+document.getElementById("enviarconsulta").addEventListener('click',function(){
+    let nomb= document.getElementById("nombreconsulta").value;
+    let correo = document.getElementById("correoconsulta").value;
+    /**console.log("El usuario a ingresado los siguientes datos: "+nomb+" "+correo);*/
+    document.getElementById("resultado").innerHTML= "Muchas gracias!, me comunicaré en breve.";
 }
+);
+
+
